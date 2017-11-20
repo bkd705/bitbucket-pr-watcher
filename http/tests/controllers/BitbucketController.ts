b659b847123fdd5331e33b8432f6b7790bddd53a
bkd705 <brennenkdenomme@gmail.com>
2017-11-19 20:48:45 -0500

    Tests

---
+++ http/tests/controllers/BitbucketController.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai'
+import { BitbucketController } from '../../controllers/BitbucketController'
+
+// describe('controller: Bitbucket', () => {
+//   describe('method: webhook', () => {
+//     it('should set ctx status to be 200', async () => {
+//       const ctx = await BitbucketController.webhook(ctx)
+//     })
+//   })
+// })
